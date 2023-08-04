9edd3fa8a53584d68295c0ca48c592d3d8899c0b
Droomiweb <140798772+Droomiweb@users.noreply.github.com>
2023-08-04 21:48:33 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -62,6 +62,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/projects" element={<Projects />} />
+           <Route path="/*" element={<DefaultRoute />} />
       </Routes>
       </div>
 
